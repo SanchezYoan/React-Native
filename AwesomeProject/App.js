a0cae8dc1d26a4f3b01b56a42798311b0bb15011
SanchezYoan <yoan30470@hotmail.fr>
2023-12-06 16:21:16 +0100

    state

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Hello World !</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.counter}>Bonjour {count}</Text>
+      <Button title="+1" onPress={() => setCount(count + 1)}></Button>
     </View>
   );
 }
@@ -16,5 +17,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  counter: {
+    fontSize: 40,
   },
 });
