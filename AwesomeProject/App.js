e7e272bb0b2c098d13bfa1c08c918a3369824a82
SanchezYoan <yoan30470@hotmail.fr>
2023-12-08 19:02:29 +0100

    object in state & Onpress event

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,25 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
-  const Person = ({ name, age }) => {
-    return (
-      <View>
-        {age ? (
-          <Text style={styles.textTwo}>
-            Je suis {name} | age: {age}
-          </Text>
-        ) : (
-          <Text style={styles.textTwo}>Je suis {name}</Text>
-        )}
-      </View>
-    );
+  const obj = {
+    name: "Spiderman",
+    age: 28,
+  };
+
+  const [infos, setInfo] = useState(obj);
+
+  const handlePress = () => {
+    setInfo({
+      name: "Batman",
+      age: 37,
+    });
   };
   return (
     <View style={styles.wrapper}>
-      <Person name="Roger" />
-      <Person name="Marie" />
-      <Person name="Steve" age="25" />
+      <Text>{infos.name}</Text>
+      <Text>{infos.age}</Text>
+      <Button
+        title="Cliquez ici"
+        color="red"
+        onPress={() => handlePress()}
+        accessibilityLabel="Learn more about this purple button"
+        disabled={false}
+      ></Button>
     </View>
   );
 }
