d73479992c65acd93bc79a9c5b2cc5b0be2433a6
SanchezYoan <yoan30470@hotmail.fr>
2023-12-08 11:29:59 +0100

    props

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -2,25 +2,32 @@ import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function App() {
+  const Person = ({ name, age }) => {
+    return (
+      <View>
+        {age ? (
+          <Text style={styles.textTwo}>
+            Je suis {name} | age: {age}
+          </Text>
+        ) : (
+          <Text style={styles.textTwo}>Je suis {name}</Text>
+        )}
+      </View>
+    );
+  };
   return (
     <View style={styles.wrapper}>
-      <View style={styles.viewOne}>
-        <Text style={styles.textOne}>Text one</Text>
-        <Text style={styles.textOne}>Text one</Text>
-        <Text style={styles.textOne}>Text one</Text>
-      </View>
-      <View style={styles.viewTwo}>
-        <Text style={styles.textTwo}>Text Two</Text>
-        <Text>Text Two</Text>
-      </View>
+      <Person name="Roger" />
+      <Person name="Marie" />
+      <Person name="Steve" age="25" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: { marginTop: 50, flexDirection: "column" },
+  wrapper: { marginTop: 50, flexDirection: "column", fontSize: 20 },
   viewOne: { backgroundColor: "green" },
-  textOne: { fontFamily: "Cochin" },
   viewTwo: { backgroundColor: "red" },
+  textOne: { fontFamily: "Cochin", fontWeight: "bold" },
   textTwo: { fontFamily: "Cochin", fontSize: 20, fontWeight: "bold" },
 });
